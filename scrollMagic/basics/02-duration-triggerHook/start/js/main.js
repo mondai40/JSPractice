7d0662c73aa4b8bde43dd7d5d3867cdee7529a53
40 daimon <tvwmdaimon40@gmail.com>
2019-12-05 11:33:30 -0800

    add reverse and each loop

--- scrollMagic/basics/02-duration-triggerHook/start/js/main.js
+++ scrollMagic/basics/02-duration-triggerHook/start/js/main.js
@@ -5,14 +5,16 @@ $(document).ready(function(){
 
 	// build a scene
 	var ourScene = new ScrollMagic.Scene({
-		triggerElement: '#project01'
+		triggerElement: '#project01 img',	//starting position
+		duration: "90%",				//animation hight % refers to view height
+		triggerHook: 0.9,				//trigger position
 	})
 	.setClassToggle('#project01', 'fade-in') // add class to project01
 	.addIndicators({
-		name: 'fade scene',
-		colorTrigger: 'black',
-		indent: 200,
-		colorStart: '#75C695'
+		name: 'fade scene', 	//common name
+		colorTrigger: 'black',	
+		colorStart: '#75C695',
+		colorEnd: "pink",
 	}) // this requires a plugin
 	.addTo(controller);
 
--- scrollMagic/basics/03-reverse-and-loop/start/js/main.js
+++ scrollMagic/basics/03-reverse-and-loop/start/js/main.js
@@ -3,18 +3,22 @@ $(document).ready(function(){
 	// Init ScrollMagic
 	var controller = new ScrollMagic.Controller();
 
-	// build a scene
-	var ourScene = new ScrollMagic.Scene({
-		triggerElement: '#project01'
-	})
-	.setClassToggle('#project01', 'fade-in') // add class to project01
-	.addIndicators({
-		name: 'fade scene',
-		colorTrigger: 'black',
-		indent: 200,
-		colorStart: '#75C695'
-	}) // this requires a plugin
-	.addTo(controller);
+	$(".project").each(function() {
+		console.log(this)
+		// build a scene
+		var ourScene = new ScrollMagic.Scene({
+			triggerElement: this.children[0],
+			triggerHook: "0.9",
+		})
+		.setClassToggle(this, 'fade-in') // add class to project01
+		.addIndicators({
+			name: 'fade scene',
+			colorTrigger: 'black',
+			indent: 200,
+			colorStart: '#75C695'
+		}) // this requires a plugin
+		.addTo(controller);
+	});
 
 });
 
