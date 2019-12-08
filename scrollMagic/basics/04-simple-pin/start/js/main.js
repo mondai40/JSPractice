3fba68a5503bb68e46e2de2e5afbc8fa2f33d1e2
40 daimon <tvwmdaimon40@gmail.com>
2019-12-07 23:39:59 -0800

    add scrollMagic pin basic

--- scrollMagic/basics/04-simple-pin/start/js/main.js
+++ scrollMagic/basics/04-simple-pin/start/js/main.js
@@ -3,13 +3,21 @@ $(document).ready(function(){
 	// Init ScrollMagic
 	var controller = new ScrollMagic.Controller();
 
+	//pin the intro
+	var pinIntro = new ScrollMagic.Scene({
+		triggerElement: "#intro",
+		triggerHook: 0,
+	})
+	.setPin("#intro")
+	.addTo(controller);
+
 	// loop through each .project element
 	$('.project').each(function(){
 
 		// build a scene
 		var ourScene = new ScrollMagic.Scene({
 			triggerElement: this.children[0],
-			triggerHook: 0.9
+			triggerHook: 0.5
 		})
 		.setClassToggle(this, 'fade-in') // add class to project01
 		.addIndicators({
