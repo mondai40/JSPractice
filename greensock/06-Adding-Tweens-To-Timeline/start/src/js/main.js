c599875eab305b0e11d90c4481fb3d2adf029516
40 daimon <tvwmdaimon40@gmail.com>
2019-12-13 14:44:39 -0800

    learnt how to use osition tweens on a timeline

--- greensock/06-Adding-Tweens-To-Timeline/start/src/js/main.js
+++ greensock/06-Adding-Tweens-To-Timeline/start/src/js/main.js
@@ -10,9 +10,10 @@
 	// Adding Tweens To A Timeline
 	tl
 		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
-		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15');
+		.add("intro")
+		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})	//1 second later after previous one
+		.from(img, 3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})			//3 seconds later after load the page, absolute
+		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, "intro+=1")	//relative
+		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut});
 
 })(jQuery);
