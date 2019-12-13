c128305db64f8984f0153c3689bd793a28c96fb3
40 daimon <tvwmdaimon40@gmail.com>
2019-12-13 15:05:46 -0800

    learnt how to control GSAP timeline

--- greensock/07-Control-Timeline-Playback/start/src/js/main.js
+++ greensock/07-Control-Timeline-Playback/start/src/js/main.js
@@ -9,12 +9,42 @@
 
 	// Controlling Timeline Playback
 	tl
-		.from(h1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
-		.add('intro')
-		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
-		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro')
-		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro+=3')
-		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, 'intro');
+		.from(h1, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(intro, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(img, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(h2, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut})
+		.from(listItem, 1, {y: -15, autoAlpha: 0, ease:Power1.easeOut});
+
+	tl.pause();
+
+	$("#btnPlay").on("click", function() {
+		tl.play();
+	});
+	$("#btnPause").on("click", function() {
+		tl.pause();
+	});
+	$("#btnResume").on("click", function() {
+		tl.resume();
+	});
+	$("#btnReverse").on("click", function() {
+		tl.reverse();
+	});
+	$("#btnSpeedUp").on("click", function() {
+		tl.timeScale(8);
+	});
+	$("#btnSlowDown").click(function() {
+		tl.timeScale(0.5);
+	});
+	$("#btnSeek").on("click", function() {
+		tl.seek(1);	//指定した時間（秒）の状態に遷移する。（1秒後の状態に遷移する）
+	});
+	$("#btnProgress").on("click", function() {
+		tl.progress(0.8); //指定した時間（%）の状態に遷移する。（50%の処理が終わった状態に遷移する）
+	});
+	$("#btnRestart").on("click", function() {
+		tl.restart(); //tl.seek(0)と同じ状態
+	});
+
 
 })(jQuery);
 
