e8188141f246d22d4ff507a73c326fed375e9e38
40 daimon <tvwmdaimon40@gmail.com>
2019-12-14 18:51:26 -0800

    learnt stagger animation

--- greensock/08-Staggering-Animations/start/src/js/main.js
+++ greensock/08-Staggering-Animations/start/src/js/main.js
@@ -14,7 +14,28 @@
 		.from(intro, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
 		.from(img, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
 		.from(h2, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
-		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15');
+		.from(listItem, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeOut}, '-=0.15')
+		// .staggerFrom(buttons, 0.2, {x: 200, ease: Power1.easeOut}, 0.1);	//0.2: staggerが完了する時間。0.1: 次のstaggerが始まる時間
+		// .staggerTo(buttons, 0.2, {x: -20, ease: Power1.easeOut}, 0.1);
+		// .staggerFromTo(
+		// 	buttons, 
+		// 	0.2, 
+		// 	{autoAlpha: 0, x: 10,},							//From position
+		// 	{autoAlpha: 1, x: -10, ease: Power1.easeOut},	//To position
+		// 	0.1);
+		.staggerFrom(
+			buttons, 
+			1, 
+			{
+				cycle: {
+					x: [50, -50],
+					scale: [2, 4],
+				},
+				autoAlpha: 0,
+				ease: Power1.easeOut,
+			}, 
+			1);
+
 
 	$('#btnPlay').on('click',function(){
 		tl.play();
