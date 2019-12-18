d33129bbbdb0863a27c166f9cef906e976272a71
40 daimon <tvwmdaimon40@gmail.com>
2019-12-18 12:15:30 -0800

    make other section pinned

--- scrollMagic/advanced/pin/01-HTML-CSS/start/js/main.js
+++ scrollMagic/advanced/pin/01-HTML-CSS/start/js/main.js
@@ -22,10 +22,18 @@ $(document).ready(function(){
     .addTo(controller);
 
     var pinScene03 = new ScrollMagic.Scene({
+        triggerElement: "#slide02",
+        triggerHook: 0,
+        duration: "200%"
+    })
+    .setPin("#slide03 .pin-wrapper")
+    .addTo(controller);
+
+    var pinScene04 = new ScrollMagic.Scene({
         triggerElement: "#slide01",
         triggerHook: 0,
         duration: "300%"
     })
-    .setPin("#slide03 .pin-wrapper")
+    .setPin("#slide04 .pin-wrapper")
     .addTo(controller);
 });
